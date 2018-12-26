46a10c1fa0e493478c876dc4f1d5fd6f3e2cb11a
Abhizzzzz <palwankar786@2gmail.com>
2018-12-26 18:49:53 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import{RouterModule,Router} from '@angular/router';
 import { AppComponent } from './app.component';
+import { UserModule } from './user/user.module';
+import { LoginComponent } from './user/login/login.component';
+import { AppService } from './app.service';
+import { ForgotPasswordComponent } from './user/forgot-password/forgot-password.component';
+import { TodoModule } from './todo/todo.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UserModule,
+    TodoModule,
+    RouterModule.forRoot([
+      {path: 'login',component: LoginComponent,pathMatch: 'full'},
+      {path: 'forgot-password/:forgotPassToken',component: ForgotPasswordComponent},
+      {path:'',redirectTo:'login',pathMatch: 'full'},
+      {path:'*',component:LoginComponent},
+      {path:'**',component:LoginComponent}
+    ])
   ],
-  providers: [],
+  providers: [AppService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
